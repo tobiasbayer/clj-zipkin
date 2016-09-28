9a290041737ac2b5a5a5c5450e8cb28620622813
Sachin <sachin@helpshift.com>
2016-09-28 17:22:17 +0530

    Added more stable start stop server methods for example

--- examples/server.clj
+++ examples/server.clj
@@ -5,14 +5,25 @@
             [clj-zipkin.tracer :as t])
   (:use [compojure.core :only (GET PUT POST ANY defroutes context)]))
 
+(defonce server nil)
 
 (def config {:scribe {:host "localhost" :port 9410}
              :service "TestService"})
+
+(defn normal-fun
+  []
+  (t/trace {:span "GET"
+            :host {:service "RendererService"}
+            :scribe (:scribe config)}
+           (do
+               (Thread/sleep 100)
+               "<h1>Hello World</h1>")))
 
 (defn main-renderer
   [request]
   ;;functions traced during request processing
   ;;can take zipking parameters from request                                      
+  (Thread/sleep 500)
   (t/trace {:span "Renderer" 
             ;;this values should be propagated over the network in
             ;;case execution continues somewhere else
@@ -32,14 +43,14 @@
 (def app (-> routes 
              (m/request-tracer config)))
 
-(def server (atom nil))
+(defn start-server [port]
+  ;; FIXME: Make this more robust by using `start-server-or-die`
+  (let [server (jetty/run-jetty
+                app
+                {:port port
+                 :join? false})]
+    (alter-var-root #'server (constantly server))))
 
-(defn stop []
-  (when @server
-    (.stop @server)))
 
-(defn start []
-  (stop)
-  (when-let [s (jetty/run-jetty app 
-                                {:join? false :port 9090})]
-    (reset! server s)))
+(defn stop-server [server]
+  (.stop ^org.eclipse.jetty.server.Server server))
